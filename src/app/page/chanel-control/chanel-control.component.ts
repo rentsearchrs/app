6455f84dc732122a605d69d72ff54e199c510567
rentsearchrs <rentsearchrs@gmail.com>
2025-02-06 12:18:12 +0100

    Update chanel-control.component.ts

--- src/app/page/chanel-control/chanel-control.component.ts
+++ src/app/page/chanel-control/chanel-control.component.ts
@@ -16,7 +16,7 @@ export class ChanelControlComponent implements OnInit {
   priceFrom?: number;
   priceTo?: number;
   locationType: string = 'all';  // Default to 'all'
-  private apiUrl = 'http://localhost:8000';  // Your FastAPI backend URL
+  private apiUrl = 'https://lviv-pject-git-main-rentsearchrs-projects.vercel.app';  // Your FastAPI backend URL
   categories = ['sent to telegram channel', 'successful'];
   typeDeals = ['kvartiry', 'doma', 'posutochno-pochasovo'];
   typeObjects = ['posutochno-pochasovo-doma', 'posutochno-pochasovo-kvartiry', 'dolgosrochnaya-arenda-kvartir', 'arenda-domov', 'prodazha-kvartir', 'prodazha-domov'];
